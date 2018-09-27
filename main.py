3f594c1db976f69bd8dc194e598c89b7da604a32
bhban <needleworm@kaist.ac.kr>
2018-09-27 17:32:32 +0900

    unmasking done

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 import post_crawler as pc
+import os
 
 feed_csv = "feed.csv"
-out_dir = "output"
+out_dir = "output_without_masking"
 
 csv = open(feed_csv)
 count = 1
 
 crawler = pc.crawler()
 
+done_list = []
+temp = os.listdir(out_dir)
+for el in temp:
+    if ".png" in el:
+        done_list.append(el)
+
 for line in csv:
-    querry = line.strip()
+    line_split = line.strip().split(",")
+    if len(line_split) != 2:
+        continue
+
+    querry = line_split[0].strip()
+    key2 = line_split[1]
+    if len(key2) < 2 :
+        continue
+    key2 = key2[1]
+    key1 = "구"
+
     if "-" in querry:
         splt = querry.split("-")
         querry = ""
         for el in splt:
             querry = querry + el
+
     if len(querry) != 13 or not querry.isdigit():
         continue
-    crawler.save_screenshot(querry, out_dir)
+
+    if querry + ".png" in done_list:
+        continue
+
+    crawler.save_screenshot_withhout_masking(querry, out_dir, key1, key2)
     print("JOB Number " + str(count) + " Done.\n>>>>>" + querry)
     count += 1
 
--- post_crawler.py
+++ post_crawler.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
+import win32api, win32con, win32gui
+import os
 
 
 def make_url(querry):
@@ -10,9 +13,9 @@ def make_url(querry):
 class crawler():
     def __init__(self):
         self.options = Options()
-        self.options.add_argument("headless")
+        #self.options.add_argument("headless")
         self.options.add_argument("window-size=848x1500")
-        self.options.add_argument("disable-gpu")
+        #self.options.add_argument("disable-gpu")
         self.driver = webdriver.Chrome(executable_path="C:/python_pkg/chromedriver.exe", chrome_options=self.options)
         #self.driver.set_window_size(848,1500)
 
@@ -23,3 +26,58 @@ class crawler():
 
     def kill(self):
         self.driver.quit()
+
+    def save_screenshot_withhout_masking(self, querry, out_dir, key1, key2):
+        url = make_url(querry)
+        self.driver.get(url)
+        self.unlock_masking(key1, key2)
+        redbox_based_awake()
+        self.driver.save_screenshot(out_dir + "/"+querry + ".png")
+
+    def unlock_masking(self, key1, key2):
+        button_location = (650, 170)
+        time.sleep(5)
+        key1_location = (185, 235)
+        key2_location = (185, 263)
+        popup_ok_location = (210, 310)
+
+        click(button_location)
+        redbox_based_sleep()
+        click(key1_location)
+        type_in(key1)
+        click(key2_location)
+        type_in(key2)
+        click(popup_ok_location)
+
+
+def click(location):
+    x, y = location
+    win32api.SetCursorPos(location)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+
+def type_in(string):
+    command = 'echo ' + string.strip() + '| clip'
+    os.system(command)
+    win32api.keybd_event(0x11, 0, 0x00, 0)
+    win32api.keybd_event(0x56, 0, 0x00, 0)
+    win32api.keybd_event(0x11, 0, 0x02, 0)
+    win32api.keybd_event(0x56, 0, 0x02, 0)
+
+
+def get_color(location):
+    x, y = location
+    return hex(win32gui.GetPixel(win32gui.GetDC(win32gui.GetActiveWindow()), x, y))
+
+
+def redbox_based_sleep():
+    redbox_location = (385, 123)
+    while get_color(redbox_location) != "0x392ddd":
+        time.sleep(0.01)
+
+
+def redbox_based_awake():
+    redbox_location = (385, 123)
+    while get_color(redbox_location) == "0x392ddd":
+        time.sleep(0.1)
