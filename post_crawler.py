dae691f33e308d6cc4efde75c116980cd82d55c7
bhban <needleworm@kaist.ac.kr>
2018-09-27 11:14:52 +0900

    headless

--- post_crawler.py
+++ post_crawler.py
@@ -10,8 +10,11 @@ def make_url(querry):
 class crawler():
     def __init__(self):
         self.options = Options()
+        self.options.add_argument("headless")
+        self.options.add_argument("window-size=848x1500")
+        self.options.add_argument("disable-gpu")
         self.driver = webdriver.Chrome(executable_path="C:/python_pkg/chromedriver.exe", chrome_options=self.options)
-        self.driver.set_window_size(848,1200)
+        #self.driver.set_window_size(848,1500)
 
     def save_screenshot(self, querry, out_dir):
         url = make_url(querry)
